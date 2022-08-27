2647043be87affd00a883c3398e0910c981c9358
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-08-27 19:48:34 +0500

    Little changes

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const STATUS = {
     TO_DO: 'to do',
     IN_PROGRESS: 'in progress',
     DONE: 'done',
-}
+};
 const PRIORITY = {
     HIGH: 'high',
     LOW: 'low',
-}
+};
 // Добавляем задачу
 function addTask(task, status, priority) {
     let newTask = {};
@@ -20,34 +20,38 @@ function addTask(task, status, priority) {
     newTask.priority = priority;
 
     list.push(newTask);
-}
+};
 // Меняем статус задачи
 function changeStatus(task, status) {
     let result = list.map(function(item) {
         if (item.name === task) {
             item.status = status;
-        }
-    })
+        };
+    });
 
     return result;
-}
+};
 // Меняем приоритетность задачи
 function changePriority(task, priority) {
     let result = list.map(function(item) {
         if (item.name === task) {
             item.priority = priority;
-        }
-    })
+        };
+    });
 
     return result;
-}
+};
 // Удаляем задачу
 function deleteTask(task) {
     
     let resultIndex = list.findIndex(item => item.name == task)
 
-    return list.splice(resultIndex, 1);
-}
+    if (resultIndex === -1) {
+        console.log('Такой задачи не существует.');
+    } else {
+        return list.splice(resultIndex, 1);
+    };
+};
 
 // Выводим список задач в консоль
 function showList() {
@@ -57,21 +61,21 @@ function showList() {
 
     list.forEach(function(item) {
         for (let key in item) {
-            if (item[key] == STATUS.IN_PROGRESS) {
+            if (item[key] === STATUS.IN_PROGRESS) {
                 inProgress = inProgress + '  ' + item.name + '\n';
             }
-            if (item[key] == STATUS.DONE) {
+            if (item[key] === STATUS.DONE) {
                 done = done + '  ' + item.name + '\n';
             }
-            if (item[key] == STATUS.TO_DO) {
+            if (item[key] === STATUS.TO_DO) {
                 toDo = toDo + '  ' + item.name + '\n';
-            }
-        }
-    })
+            };
+        };
+    });
     let result = toDo + inProgress + done;
                 
     return console.log(result);
-}
+};
 
 addTask('drink coffee', STATUS.IN_PROGRESS, PRIORITY.HIGH);
 addTask('have a walk', STATUS.IN_PROGRESS, PRIORITY.HIGH);
